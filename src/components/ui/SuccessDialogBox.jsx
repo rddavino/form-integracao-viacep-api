e8331d36be990f8df9ba788db114593f51f75514
rddavino <rd.davino@gmail.com>
2024-08-16 18:24:30 -0300

    feat(SuccessDialogBox): change component structure and add style using Headless UI components

--- src/components/ui/SuccessDialogBox.jsx
+++ src/components/ui/SuccessDialogBox.jsx
@@ -1,5 +1,11 @@
 import { React, useState, forwardRef, useImperativeHandle } from "react";
 import Button from "./button";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
 
 const SuccessDialogBox = forwardRef(({ dialogBoxText, dialogBoxHeader }, ref) => {
   const [open, setOpen] = useState(false)
@@ -10,38 +16,66 @@ const SuccessDialogBox = forwardRef(({ dialogBoxText, dialogBoxHeader }, ref) =>
       setOpen(true);
     },
     close() {
-      setIsOpen(false);
+      setOpen(false);
+      console.log("chamou");
     },
   }));
   return (
-    isOpen && (
-      <div className="w-full h-full bg-white absolute z-10">
-        <div className="w-12/12 h-12 bg-red-300">
-          {modalHeader}
-          {modalText}
-        </div>
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-black bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
 
-        <Button
-          className="w-full 
-                rounded-full
-                bg-yellow-500 
-                h-12 
-                text-center 
-                justify-center
-                text-teal-900 
-                text-base 
-                uppercase
-                font-bold
-                no-underline
-                flex
-                items-center"
-          type="text"
-          onClick={() => setIsOpen(false)}
-        >
-          Close
-        </Button>
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+          >
+            <div className="bg-white p-10 lg:px-20">
+              <div className="mb-5">
+                <div className="mt-3 flex flex-col align-middle">
+                <div className="h-12 w-12 mb-5">
+                  <img src="src/assets/images/check-mark.png" alt="" />
+                </div>
+                  <DialogTitle
+                    as="h3"
+                    className="text-lg text-center font-semibold leading-6 text-gray-900"
+                  >
+                    {dialogBoxHeader}
+                  </DialogTitle>
+                  <div className="mt-2">
+                    <p className="text-sm text-center text-gray-500">{dialogBoxText}</p>
+                  </div>
+                </div>
+              </div>
+              <Button
+                type="button"
+                data-autofocus
+                onClick={() => setOpen(false)}
+                className="w-full 
+                        rounded-full
+                        bg-teal-900
+                        h-12 
+                        text-center 
+                        justify-center
+                        text-white 
+                        text-base 
+                        uppercase
+                        font-bold
+                        no-underline
+                        flex
+                        items-center"
+              >
+                Entendi!
+              </Button>
+            </div>
+            
+          </DialogPanel>
+        </div>
       </div>
-    )
+    </Dialog>
   );
 });
 
