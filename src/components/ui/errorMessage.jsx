13070f44d1232c49ef3a62741038d5d62822b2aa
rddavino <rd.davino@gmail.com>
2024-08-16 07:30:55 -0300

    feat(ui): add error message component to the project

---
+++ src/components/ui/errorMessage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+const ErrorMessage = ({ fieldName }) => {
+  const {
+    formState: { errors }
+  } = useFormContext();
+
+  return (
+    errors[fieldName] && (
+      <p className="text-red-500 text-sm p-0 m-0">
+        {errors[fieldName].message}
+      </p>
+    )
+  );
+};
+
+export default ErrorMessage;
