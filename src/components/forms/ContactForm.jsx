f9db868573b740e2df70537bb77d4881d21aef18
rddavino <rd.davino@gmail.com>
2024-08-16 20:36:18 -0300

    refactor: remove unused variables to clean up code

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -1,12 +1,11 @@
-import * as Yup from "yup";
+import { useRef } from "react";
 import { useForm, FormProvider } from "react-hook-form";
+import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import AddressForm from "./AddressForm";
 import PersonalInfoForm from "./PersonalInfoForm";
 import Button from "../ui/button";
 import SuccessDialogBox from "../ui/SuccessDialogBox";
-import React, { useRef } from 'react';
-
 
 const validations = Yup.object().shape({
   firstName: Yup.string()
@@ -55,24 +54,29 @@ function ContactForm() {
       bairro: "",
       localidade: "",
     });
-  };  
-  const dialogBoxHeader = "Você está um passo mais próximo de realizar seu sonho!"
-  const dialogBoxText = "Suas informações foram enviadas com sucesso." 
+  };
+  const dialogBoxHeader =
+    "Você está um passo mais próximo de realizar seu sonho!";
+  const dialogBoxText = "Suas informações foram enviadas com sucesso.";
   return (
     <>
-      <SuccessDialogBox ref={modalRef} dialogBoxHeader={dialogBoxHeader} dialogBoxText={dialogBoxText}/>
+      <SuccessDialogBox
+        ref={modalRef}
+        dialogBoxHeader={dialogBoxHeader}
+        dialogBoxText={dialogBoxText}
+      />
       <div className="flex justify-center align-middle">
-        <div className="max-w-md">
-        <p className="text-white mb-5 font-bold text-center">
-          O Minha Casa Minha Vida facilita a conquista do seu apartamento! Dê o
-          primeiro passo para sair do aluguel!
-        </p>
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <PersonalInfoForm />
-            <AddressForm />
-            <Button
-              className="w-full 
+        <div className="max-w-sm sm:px-5 my-10">
+          <p className="text-white mb-5 font-bold text-center">
+            O Minha Casa Minha Vida facilita a conquista do seu apartamento! Dê
+            o primeiro passo para sair do aluguel!
+          </p>
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)}>
+              <PersonalInfoForm />
+              <AddressForm />
+              <Button
+                className="w-full 
                         rounded-full
                         bg-yellow-500 
                         h-12 
@@ -85,12 +89,12 @@ function ContactForm() {
                         no-underline
                         flex
                         items-center"
-              type="submit"
-            >
-              Enviar
-            </Button>
-            <Button
-              className="w-full 
+                type="submit"
+              >
+                Enviar
+              </Button>
+              <Button
+                className="w-full 
                         rounded-full
                         bg-none
                         h-12 
@@ -103,15 +107,14 @@ function ContactForm() {
                         no-underline
                         flex
                         items-center"
-              type="button"
-              onClick={onReset}
-            >
-              Limpar
-            </Button>
-          </form>
-        </FormProvider>
+                type="button"
+                onClick={onReset}
+              >
+                Limpar
+              </Button>
+            </form>
+          </FormProvider>
         </div>
-
       </div>
     </>
   );
--- src/components/ui/SuccessDialogBox.jsx
+++ src/components/ui/SuccessDialogBox.jsx
@@ -1,4 +1,4 @@
-import { React, useState, forwardRef, useImperativeHandle } from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
 import Button from "./button";
 import {
   Dialog,
@@ -7,54 +7,56 @@ import {
   DialogTitle,
 } from "@headlessui/react";
 
-const SuccessDialogBox = forwardRef(({ dialogBoxText, dialogBoxHeader }, ref) => {
-  const [open, setOpen] = useState(false)
+const SuccessDialogBox = forwardRef(
+  ({ dialogBoxText, dialogBoxHeader }, ref) => {
+    const [open, setOpen] = useState(false);
 
+    useImperativeHandle(ref, () => ({
+      open() {
+        setOpen(true);
+      },
+      close() {
+        setOpen(false);
+        console.log("chamou");
+      },
+    }));
+    return (
+      <Dialog open={open} onClose={setOpen} className="relative z-10">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-black bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+        />
 
-  useImperativeHandle(ref, () => ({
-    open() {
-      setOpen(true);
-    },
-    close() {
-      setOpen(false);
-      console.log("chamou");
-    },
-  }));
-  return (
-    <Dialog open={open} onClose={setOpen} className="relative z-10">
-      <DialogBackdrop
-        transition
-        className="fixed inset-0 bg-black bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
-      />
-
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <DialogPanel
-            transition
-            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
-          >
-            <div className="bg-white p-10 lg:px-20">
-              <div className="mb-5">
-                <div className="mt-3 flex flex-col align-middle items-center">
-                <div className="h-12 w-12 mb-5">
-                  <img src="src/assets/images/check-mark.png" alt="" />
-                </div>
-                  <DialogTitle
-                    as="h3"
-                    className="text-lg text-center font-semibold leading-6 text-gray-900"
-                  >
-                    {dialogBoxHeader}
-                  </DialogTitle>
-                  <div className="mt-2">
-                    <p className="text-sm text-center text-gray-500">{dialogBoxText}</p>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <DialogPanel
+              transition
+              className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            >
+              <div className="bg-white p-10 lg:px-20">
+                <div className="mb-5">
+                  <div className="mt-3 flex flex-col align-middle items-center">
+                    <div className="h-12 w-12 mb-5">
+                      <img src="./src/assets/images/check-mark.png" alt="" />
+                    </div>
+                    <DialogTitle
+                      as="h3"
+                      className="text-lg text-center font-semibold leading-6 text-gray-900"
+                    >
+                      {dialogBoxHeader}
+                    </DialogTitle>
+                    <div className="mt-2">
+                      <p className="text-sm text-center text-gray-500">
+                        {dialogBoxText}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <Button
-                type="button"
-                data-autofocus
-                onClick={() => setOpen(false)}
-                className="w-full 
+                <Button
+                  type="button"
+                  data-autofocus
+                  onClick={() => setOpen(false)}
+                  className="w-full 
                         rounded-full
                         bg-teal-900
                         h-12 
@@ -67,16 +69,16 @@ const SuccessDialogBox = forwardRef(({ dialogBoxText, dialogBoxHeader }, ref) =>
                         no-underline
                         flex
                         items-center"
-              >
-                Entendi!
-              </Button>
-            </div>
-            
-          </DialogPanel>
+                >
+                  Entendi!
+                </Button>
+              </div>
+            </DialogPanel>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  );
-});
+      </Dialog>
+    );
+  }
+);
 
 export default SuccessDialogBox;
