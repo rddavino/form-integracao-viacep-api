69659a1e215fde3ea18026a26eb37ad5cda749c9
rddavino <rd.davino@gmail.com>
2024-08-16 07:30:25 -0300

    feat(ui): add input field component to the project

---
+++ src/components/ui/inputField.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+const InputField = ({ type, fieldName, placeholder, disabled }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <input
+      className={`w-full bg-stone-700 pl-3 pr-3 h-12 text-white rounded-md border-none font-bold
+        ${disabled ? "bg-opacity-20 text-opacity-30" : ""} ${errors ? "ring-offset-0 ring-2 ring-red-500" : ""}`}
+      type={type}
+      placeholder={placeholder}
+      {...register(`${fieldName}`)}
+      disabled={disabled === true ? true : false}
+    />
+  );
+};
+
+export default InputField;
